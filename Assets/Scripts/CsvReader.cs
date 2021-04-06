58dcd6b256aca74d47754abb0cd27d07b3eaadcb
tarazhong <zhong.tara@gmail.com>
2021-04-07 00:02:14 +0200

    fixed webgl tsv reader fskdlfopsfkqosf

--- Assets/Scripts/CsvReader.cs
+++ Assets/Scripts/CsvReader.cs
@@ -2,6 +2,7 @@
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class CsvReader : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class CsvReader : MonoBehaviour
     public void InitCsvParser(GameManager manager)
     {
         //Get the path of the Game data folder
-        string m_Path = Application.dataPath + "/Resources/dialogues.tsv";
+        //string m_Path = Application.dataPath + "/Resources/dialogues.tsv";
+        //string m_Path = Path.Combine(Application.streamingAssetsPath, "dialogues.tsv");
 
         //Output the Game data path to the console
         //Debug.Log("Path : " + m_Path);
@@ -17,32 +19,42 @@ public class CsvReader : MonoBehaviour
         // Care not to open the csv file (in excel or other app) when launching script
         // Check that your file is UTF 8 encoded 
 
-        ReadActions(manager,m_Path);
+
+        StartCoroutine(loadStreamingAsset("dialogues.tsv", manager));
     }
 
-    private void ReadActions(GameManager manager, string m_Path)
+    IEnumerator loadStreamingAsset(string fileName, GameManager manager)
+    {
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+
+        string result;
+        if (filePath.Contains("://") || filePath.Contains(":///"))
+        {
+            WWW www = new WWW(filePath);
+            yield return www;
+            result = www.text;
+        }
+        else
+            result = System.IO.File.ReadAllText(filePath);
+
+        ReadActions(manager, result);
+    }
+
+    private void ReadActions(GameManager manager, string text)
     {
         // initData
         manager.dialoguesData = new List<DialogueData>();
-        StreamReader reader = new StreamReader(m_Path);
-        string line;
+
         //Define separator pattern
-        Regex CSVParser = new Regex("\t"); // (",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-
-        // Skip 1st line
-        if ((line = reader.ReadLine()) != null)     {        }
-
-        // Read file until end of file
-        while ((line = reader.ReadLine()) != null) // Foreach lines in the document
+        var dataLines = text.Split('\n');
+        for (int i = 1; i < dataLines.Length; i++)
         {
-            //Separating columns to array
-            string[] rowData = CSVParser.Split(line);
+            var rowData = dataLines[i].Split('\t');
             if (rowData[0] == "")
             {
                 Debug.Log("skip");
                 continue;
             }
-            //Debug.Log(line);
             DialogueData dialogue = new DialogueData(rowData[0], rowData[1], rowData[2], rowData[3], rowData[4], rowData[5], rowData[6], rowData[7], rowData[8], rowData[9], rowData[10], rowData[11], rowData[12], rowData[13], rowData[14]);
             manager.dialoguesData.Add(dialogue);
         }
